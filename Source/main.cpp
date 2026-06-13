cc2f9db1886b36c73a0a9bdba539f2004f6e965d
Amirali11211 <145267230+Amirali11211@users.noreply.github.com>
2026-06-13 06:03:29 -1200

    Create main.cpp

---
+++ Source/main.cpp
@@ -0,0 +1,10 @@
+#include"..\Headers\DataStructure.h"
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	Branch test(0,"test");
+	cout<<"This is a test: id: "<<test.getId()<<" "<<"name: "<<test.getName()<<endl;
+	return 0;
+}
