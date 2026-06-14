46a8d35536166b7ca1acff1210a6764dd06d9939
amirrezakhaksar-ut <amirrezakhaksar8@gmail.com>
2026-06-14 10:51:25 +0330

    Add files via upload

--- Headers/DataStructure.cpp
+++ Headers/DataStructure.cpp
@@ -4,28 +4,41 @@ using namespace std;
 
 Branch::Branch(){}
 Branch::Branch(int branchId,string branchName):id(branchId),name(branchName){}
-const int Branch::getId()const{
+int Branch::getId()const{
 	return id;
 }
+
 const string Branch::getName()const{
 	return name;
 }
-const void Branch::setName(string newName){
+void Branch::setName(string newName){
 	name = newName;
 }
 
 
 Account::Account(){}
-Account::Account(int accId,string accName, string accNumber):id(accId),name(accName),number(accNumber){}
-const int Account::getId()const{
+Account::Account(int accId, string accName, string accNumber, string accPassword, int accBranchId, double accBalance, bool accActive):
+	id(accId),
+	name(accName),
+	number(accNumber),
+	password_hash(accPassword),
+	branch_id(accBranchId),
+	balance(accBalance),
+	active(accActive)
+{}
+
+int Account::getId()const{
 	return id;
 }
+
 const string Account::getName()const{
 	return name;
 }
-const void Account::setName(string newName){
+
+void Account::setName(string newName){
 	name = newName;
 }
+
 const string Account::getNumber()const{
 	return number;
 }
